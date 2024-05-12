2b28a53d6bda26a515f701b022f4bfdd7a54a152
letantai3001180588 <54881497+letantai3001180588@users.noreply.github.com>
2024-05-12 22:50:38 +0700

    style: footer

--- src/components/footer.tsx
+++ src/components/footer.tsx
@@ -1,3 +1,23 @@
+
+const footer: { title: string, desc: string[] }[] = [
+    {
+        title: "Services",
+        desc: ["Company Review", "Accounts Review", "HR Consulting", "SEO Optimisation"]
+    },
+    {
+        title: "Company",
+        desc: ["About", "Meet the Team", "Accounts Review"]
+    },
+    {
+        title: "Helpful Links",
+        desc: ["Contact", "FAQs", "Live Chat"]
+    },
+    // {
+    //     title: "Legal",
+    //     desc: ["Return Policy", "Privacy Policy", "Payment Policy", "Shipping Policy"]
+    // },
+]
+
 export default function Footer() {
     return <footer className="bg-white dark:bg-gray-900">
         <div className="mx-auto w-full max-w-screen-xl p-4 py-6 lg:py-8">
@@ -9,44 +29,23 @@ export default function Footer() {
                     </a>
                 </div>
                 <div className="grid grid-cols-2 gap-8 sm:gap-6 sm:grid-cols-3">
-                    <div>
-                        <h2 className="mb-6 text-sm font-semibold text-gray-900 uppercase dark:text-white">Client</h2>
-                        <ul className="text-gray-500 dark:text-gray-400 font-medium">
-                            <li className="mb-4">
-                                <a href="" className="hover:underline">Care</a>
-                            </li>
-                            <li>
-                                <a href="" className="hover:underline">Payment</a>
-                            </li>
-                        </ul>
-                    </div>
-                    <div>
-                        <h2 className="mb-6 text-sm font-semibold text-gray-900 uppercase dark:text-white">Follow us</h2>
-                        <ul className="text-gray-500 dark:text-gray-400 font-medium">
-                            <li className="mb-4">
-                                <a href="" className="hover:underline ">Github</a>
-                            </li>
-                            <li>
-                                <a href="" className="hover:underline">Discord</a>
-                            </li>
-                        </ul>
-                    </div>
-                    <div>
-                        <h2 className="mb-6 text-sm font-semibold text-gray-900 uppercase dark:text-white">Legal</h2>
-                        <ul className="text-gray-500 dark:text-gray-400 font-medium">
-                            <li className="mb-4">
-                                <a href="#" className="hover:underline">Privacy Policy</a>
-                            </li>
-                            <li>
-                                <a href="#" className="hover:underline">Terms &amp; Conditions</a>
-                            </li>
-                        </ul>
-                    </div>
+                    {footer.map((item: { title: string, desc: string[] }) =>
+                        <div>
+                            <h2 className="mb-6 text-sm font-semibold text-gray-900 uppercase dark:text-white">{item.title}</h2>
+                            <ul className="text-gray-500 dark:text-gray-400 font-medium">
+                                {item.desc.map((desc: string) =>
+                                    <li className="mb-4">
+                                        <a href="" className="hover:underline">{desc}</a>
+                                    </li>
+                                )}
+                            </ul>
+                        </div>
+                    )}
                 </div>
             </div>
             <hr className="my-6 border-gray-200 sm:mx-auto dark:border-gray-700 lg:my-8" />
             <div className="sm:flex sm:items-center sm:justify-between">
-                <span className="text-sm text-gray-500 sm:text-center dark:text-gray-400">© 2023 <a href="" className="hover:underline">Flowbite™</a>. All Rights Reserved.
+                <span className="text-sm text-gray-500 sm:text-center dark:text-gray-400">© 2024 <a href="" className="hover:underline">LeTanTai™</a>. All Rights Reserved.
                 </span>
                 <div className="flex mt-4 sm:justify-center sm:mt-0">
                     <a href="#" className="text-gray-500 hover:text-gray-900 dark:hover:text-white">
