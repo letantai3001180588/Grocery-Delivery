4bfa09ab450c055a44d8987be8aba13bd12d4f3c
Lê Tấn Tài <54881497+letantai3001180588@users.noreply.github.com>
2024-05-17 22:59:57 +0700

    style: detail product

--- src/pages/DetailProduct/index.tsx
+++ src/pages/DetailProduct/index.tsx
@@ -1,8 +1,10 @@
+import Footer from "components/footer";
 import Navbar from "components/navbar";
 
 export default function DetailProduct() {
     return <>
         <Navbar />
         Detail Product
+        <Footer />
     </>
 }
