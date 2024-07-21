785567471b6abac883e6c34fb965ac58c1ee1b16
Lê Tấn Tài <54881497+letantai3001180588@users.noreply.github.com>
2024-07-21 23:06:01 +0700

    update: product detail

--- src/pages/DetailProduct/index.tsx
+++ src/pages/DetailProduct/index.tsx
@@ -5,7 +5,7 @@ export default function DetailProduct() {
     return <>
         <Navbar />
             <div>Image:</div>
-            <div>Product 22</div>
+            <div>Product 23</div>
             <div>Description:</div>
             <div>Price:</div>
             <div>Cateagory:</div>
